23ec75d1fb1c060ac38ecf8c8f4c35edaf4336c1
Paul Trampert <paul.trampert@ptrampert.com>
2020-01-12 22:39:35 -0500

    Can render a page now.

---
+++ TCloud.Web/scripts/App.jsx
@@ -0,0 +1,44 @@
+﻿import React from 'react';
+import {
+  HashRouter as Router,
+  Route,
+  NavLink as Link,
+  Switch
+} from 'react-router-dom';
+import { Nav } from '@ptrampert/flatitude';
+
+
+class App extends React.Component {
+  render = () => {
+    return (
+      <Router>
+        <div className="app left-nav">
+          <header>
+            <h3>TCLoud</h3>
+            <button className="transparent nav-toggle" onClick={() => this.setNavCollapsed(!this.state.navCollapsed)}><i className="fa fa-bars"></i></button>
+          </header>
+          <Nav>
+            <Link to="/" exact activeClassName="current">Dashboard</Link>
+            <Link to="/movies" activeClassName="current">Movies</Link>
+            <Link to="/music" activeClassName="current">Music</Link>
+          </Nav>
+          <main>
+            <Switch>
+              <Route path="/movies">
+              Movies
+              </Route>
+              <Route path="/music">
+                Music
+              </Route>
+              <Route path="/">
+              Home
+              </Route>
+            </Switch>
+          </main>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
--- TCloud.Web/scripts/index.js
+++ TCloud.Web/scripts/index.js
@@ -1,1 +1,12 @@
 ﻿import '@babel/polyfill';
+import '@ptrampert/flatitude';
+import App from './App.jsx';
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const e = React.createElement;
+
+let container = document.createElement('div');
+document.body.style.height = '100%';
+document.body.append(container);
+ReactDOM.render(e(App), container);
